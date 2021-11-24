4a8f302450c318beb738ebb065519817e9f7eda8
Jakub Wiselka <jakub.wiselka@oms-pruefservice.de>
2021-11-24 23:05:56 +0100

    Code clead and token file added

--- bot.py
+++ bot.py
@@ -9,52 +9,63 @@ from text import *
 TOKEN = open("token.txt", "r").read()
 DB = f'{os.path.dirname(os.path.abspath(__file__))}/telebot.db'
 bot = telebot.TeleBot(TOKEN)
-logging.basicConfig(filename=f'{os.path.dirname(os.path.abspath(__file__))}/logs.log', level=logging.WARNING)
+logging.basicConfig(
+    filename=f'{os.path.dirname(os.path.abspath(__file__))}/logs.log', level=logging.WARNING)
+
+if not TOKEN:
+    print("Please add bot token!")
+    exit()
 
 
 def _get_table_name(chat_id):
     return f'group{str(chat_id).replace("-", "_")}'
 
+
 def _check_if_table_exist(chat_id):
     table = _get_table_name(chat_id)
     conn = sqlite3.connect(DB)
     c = conn.cursor()
-    c.execute(f"""SELECT name FROM sqlite_master WHERE type='table' AND name='{table}';""")
+    c.execute(
+        f"""SELECT name FROM sqlite_master WHERE type='table' AND name='{table}';""")
     exits = c.fetchone()
     conn.close()
     return True if exits else False
+
 
 def _create_new_table(chat_id):
     table = _get_table_name(chat_id)
     conn = sqlite3.connect(DB)
     c = conn.cursor()
-    c.execute(f"""CREATE TABLE {table} (id INTEGER PRIMARY KEY AUTOINCREMENT, product TEXT)""")
+    c.execute(
+        f"""CREATE TABLE {table} (id INTEGER PRIMARY KEY AUTOINCREMENT, product TEXT)""")
     conn.commit()
     conn.close()
 
+
 def _activate_bot(msg):
-    bot.send_message(msg.chat.id, ACTIVATE_BOT , parse_mode="html")
+    bot.send_message(msg.chat.id, ACTIVATE_BOT, parse_mode="html")
+
 
 def welcome_message(msg):
-    bot.send_message(msg.chat.id, f"Hi {msg.from_user.first_name.title()} 👋\n" + WELCOME, parse_mode="html")
+    bot.send_message(
+        msg.chat.id, f"Hi {msg.from_user.first_name.title()} 👋\n" + WELCOME, parse_mode="html")
 
 
 def confirm():
     markup = types.InlineKeyboardMarkup()
     markup.add(
         types.InlineKeyboardButton(
-            text = NO,
+            text=NO,
             callback_data=f"no"
         ),
         types.InlineKeyboardButton(
-            text = YES,
+            text=YES,
             callback_data=f"yes"
         )
     )
     return markup
 
 
-# DELETE
 @bot.message_handler(commands=['clear'])
 def command_delete(msg):
     if _check_if_table_exist(msg.chat.id):
@@ -63,6 +74,7 @@ def command_delete(msg):
         if not _check_if_table_exist(msg.chat.id):
             _activate_bot(msg)
             return
+
 
 @bot.callback_query_handler(func=lambda call: call.data in ["no", 'yes'])
 def command_delete_handler(call):
@@ -74,10 +86,10 @@ def command_delete_handler(call):
         c = conn.cursor()
         c.execute(f"DELETE FROM {table};")
         conn.commit()
-        bot.edit_message_text(chat_id=call.message.chat.id,text=CLEAR ,message_id=call.message.message_id)
+        bot.edit_message_text(chat_id=call.message.chat.id,
+                              text=CLEAR, message_id=call.message.message_id)
 
 
-# HELP
 @bot.message_handler(commands=['help'])
 def command_help(msg):
     if _check_if_table_exist(msg.chat.id):
@@ -87,7 +99,7 @@ def command_help(msg):
             _activate_bot(msg)
             return
 
-# START
+
 @bot.message_handler(commands=['start'])
 def command_start(msg):
     if _check_if_table_exist(msg.chat.id):
@@ -96,7 +108,7 @@ def command_start(msg):
         _create_new_table(msg.chat.id)
         welcome_message(msg)
 
-# LIST
+
 @bot.message_handler(commands=['list'])
 def command_list(msg, edit_mode=False):
     if not _check_if_table_exist(msg.chat.id):
@@ -112,23 +124,24 @@ def command_list(msg, edit_mode=False):
         return
     len_nr = len(str(len(list)))
     text = ""
-    for i,x in enumerate(list):
+    for i, x in enumerate(list):
         text += f"<code>{i + 1:{len_nr}d}. <b>{x[0]}x</b>-</code> {html.escape(x[1].title())}\n"
     if edit_mode:
-        bot.send_message(msg.chat.id, text, reply_markup=makeKeyboard(list), parse_mode="html")
+        bot.send_message(msg.chat.id, text,
+                         reply_markup=makeKeyboard(list), parse_mode="html")
     else:
         bot.send_message(msg.chat.id, text, parse_mode="html")
 
-# ADD
+
 @bot.message_handler(commands=['add'])
 def command_add(msg):
-    ## add exeption when table not exist
     if not _check_if_table_exist(msg.chat.id):
         _activate_bot(msg)
         return
     try:
         text = msg.text[5:]
-        products = ",".join('("' + p.strip().lower() + '")' for p in text.split(',') if p)
+        products = ",".join(
+            '("' + p.strip().lower() + '")' for p in text.split(',') if p)
         if not products:
             return
         table = _get_table_name(msg.chat.id)
@@ -141,14 +154,13 @@ def command_add(msg):
         bot.reply_to(msg, WRONG)
     conn.close()
 
-# EDIT
+
 @bot.message_handler(commands=['edit'])
 def command_edit(msg):
     if not _check_if_table_exist(msg.chat.id):
         _activate_bot(msg)
         return
     command_list(msg, edit_mode=True)
-
 
 
 def makeKeyboard(list):
@@ -158,30 +170,31 @@ def makeKeyboard(list):
         markup.add(
             types.InlineKeyboardButton(
                 # Spaces added to alight text to left side
-                text = f"{REMOVE}{p[1].title()}{' '*50}.",
+                text=f"{REMOVE}{p[1].title()}{' '*50}.",
                 callback_data=f"{i},del"
             ),
-             types.InlineKeyboardButton(
-                text = MINUS,
+            types.InlineKeyboardButton(
+                text=MINUS,
                 callback_data=f"{i},rm"
             ),
             types.InlineKeyboardButton(
-                text = PLUS,
+                text=PLUS,
                 callback_data=f"{i},add"
             ),
         )
     markup.add(
         types.InlineKeyboardButton(
-            text = CLOSE,
+            text=CLOSE,
             callback_data=f"0,clo"
         ),
         types.InlineKeyboardButton(
-            text = REFRESH,
+            text=REFRESH,
             callback_data=f"0,ref"
         )
     )
 
     return markup
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_query(call):
@@ -194,10 +207,12 @@ def handle_query(call):
         id, action = call.data.split(',')
         id = int(id)
         if action == "add":
-            c.execute(f"INSERT INTO {table} (product) VALUES ('{list[id][1]}');")
+            c.execute(
+                f"INSERT INTO {table} (product) VALUES ('{list[id][1]}');")
             conn.commit()
         elif action == "rm":
-            c.execute(f"DELETE FROM {table} WHERE product = '{list[id][1]}' LIMIT 1;")
+            c.execute(
+                f"DELETE FROM {table} WHERE product = '{list[id][1]}' LIMIT 1;")
             conn.commit()
         elif action == "del":
             c.execute(f"DELETE FROM {table} WHERE product = '{list[id][1]}';")
@@ -208,68 +223,19 @@ def handle_query(call):
 
         len_nr = len(str(len(list)))
         text = ""
-        for i,x in enumerate(list):
+        for i, x in enumerate(list):
             text += f"<code>{i + 1:{len_nr}d}. <b>{x[0]}x</b>-</code> {html.escape(x[1].title())}\n"
 
         bot.edit_message_text(
             chat_id=call.message.chat.id,
             text=text if text else EMPTY,
             message_id=call.message.message_id,
-            reply_markup=None if not list or action == "clo" else makeKeyboard(list),
+            reply_markup=None if not list or action == "clo" else makeKeyboard(
+                list),
             parse_mode='HTML'
         )
     except Exception as e:
         logging.warning(e)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # default handler for every other text
-# @bot.message_handler(func=lambda message: True)
-# def command_default(msg):
-#     if not _check_if_table_exist(msg.chat.id):
-#         return
-#     try:
-#         # validate number
-#         next_number = int(msg.text)
-#     except Exception:
-#         bot.delete_message(msg.chat.id, msg.message_id)
-#         return
-    
-#     # check if number correct
-#     table = _get_table_name(msg.chat.id)
-#     conn = sqlite3.connect(DB)
-#     c = conn.cursor()
-#     # c.execute(f"SELECT id FROM {table} ORDER BY id DESC LIMIT 1")
-#     c.execute(f"SELECT * FROM {table} ORDER BY id DESC LIMIT 1")
-#     previous = c.fetchone()
-#     if previous[0] + 1 == next_number and previous[1] != msg.from_user.id:
-#         c.execute(f"INSERT INTO {table} (author_id) VALUES ({msg.from_user.id})")
-#         conn.commit()
-#     else:
-#         bot.delete_message(msg.chat.id, msg.message_id)
-#     c = conn.close()
 
 bot.polling()
